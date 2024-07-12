d5eda1b8ae52c474ccea7d61280f1fca2da4cdad
KonstantinShaltyrev <ska1buzon@gmail.com>
2024-07-12 13:23:25 +0300

    v2.0

--- src/main/java/web/exceptions/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package web.exceptions;
-
-public class UserNotFoundException extends RuntimeException {
-
-    public UserNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
